0a925f56521645a7d29589412604f3de16bba403
Aaron24000 <aaron.nobles@outlook.com>
2021-04-27 16:33:35 -0400

    Sorted validation out!

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const postController = require('../controllers/post')
+const postController = require('../controllers/post');
+const validator = require('../validation/index');
 
 const router = express.Router();
 
 router.get('/', postController.getPosts);
+
+router.post('/post', validator.createPostValidator, postController.createPost);
 
 module.exports = router;
